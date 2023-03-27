924c19e1272b64ac356a2129c5bb238f107221d8
StiltonBree <nruth@g.clemson.edu>
2023-03-27 11:51:03 -0700

    You can now click the button to increase score!

--- main.py
+++ main.py
@@ -22,8 +22,13 @@ font = pygame.font.SysFont('comicsansms',30)
 #button color
 buttonColor = (70,80,90)
 
+#default button text
 text = font.render('Click!', True, (255,255,255))
 
+#variable to update score
+score = 0
+
+scoreText = font.render('Score: {score} ', True, (255,255,255))
 #Game Loop for each event
 # adding sys.exit gets rid of the video system not initialized error
 while True:
@@ -32,14 +37,20 @@ while True:
             pygame.quit()
             sys.exit()
         if ev.type == pygame.MOUSEBUTTONDOWN:
-            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-                pygame.quit()
+            if 360 - 80 <= mouse[0] <= 360+80 and 520 - 80 <= mouse[1] <= 520+80:
+                #change this to update score for clicker
+                score+=1
                     
     screen.fill((0,0,55))
-    
+    #mouse position
     mouse = pygame.mouse.get_pos()
     
     buttonCircle = pygame.draw.circle(screen,buttonColor, (360,520), 80.0)
     #text over button
     screen.blit(text,(buttonCircle.centerx - 30,buttonCircle.centery - 30))
+    
+    #score text, need to render text before blitting
+    scoreText = font.render(f'Score: {score} ', True, (255,255,255))
+    screen.blit(scoreText,(width/2-100,0))
+    
     pygame.display.update()
