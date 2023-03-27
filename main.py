6b83565626422251fba2ce93934e430448831137
StiltonBree <nruth@g.clemson.edu>
2023-03-27 11:30:16 -0700

    Got Click Text onto Button

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ window = (720,720)
 #window renderer
 screen = pygame.display.set_mode((window))
 
+#dimensions of screen
+width = screen.get_width()
+height = screen.get_height()
+
 #bool for running to avoid video errors
 running = True
 
@@ -17,6 +21,9 @@ font = pygame.font.SysFont('comicsansms',30)
 
 #button color
 buttonColor = (70,80,90)
+
+text = font.render('Click!', True, (255,255,255))
+
 #Game Loop for each event
 # adding sys.exit gets rid of the video system not initialized error
 while True:
@@ -24,9 +31,15 @@ while True:
         if ev.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if ev.type == pygame.MOUSEBUTTONDOWN:
+            if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
+                pygame.quit()
+                    
     screen.fill((0,0,55))
     
     mouse = pygame.mouse.get_pos()
     
-    pygame.draw.circle(screen,buttonColor, (360,520), 80.0)
+    buttonCircle = pygame.draw.circle(screen,buttonColor, (360,520), 80.0)
+    #text over button
+    screen.blit(text,(buttonCircle.centerx - 30,buttonCircle.centery - 30))
     pygame.display.update()
