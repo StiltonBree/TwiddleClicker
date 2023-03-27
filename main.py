83891b7d24f0b88b1f9b37b29cc1274988ff3288
StiltonBree <nruth@g.clemson.edu>
2023-03-27 10:45:49 -0700

    Got screen window to render color background

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ pygame.init()
 
 #initial window dimensions
 window = (720,720)
-
 #window renderer
-screen = pygame.display.set_mode(window)
+screen = pygame.display.set_mode((window))
+
+#bool for running to avoid video errors
+running = True
+
+#font from sysfont
+font = pygame.font.SysFont('comicsansms',30)
 
 #Game Loop for each event
+# adding sys.exit gets rid of the video system not initialized error
 while True:
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             pygame.quit()
-
-#change window color
+            sys.exit()
+    screen.fill((0,0,55))
+    pygame.display.update()
