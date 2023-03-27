e898dd35cd0ea3521e5554f5b2be9be9d0180819
StiltonBree <nruth@g.clemson.edu>
2023-03-27 10:28:53 -0700

    Made a window

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pygame
+import sys
 
+pygame.init()
 
+#initial window dimensions
+window = (720,720)
+
+#window renderer
+screen = pygame.display.set_mode(window)
+
+#Game Loop for each event
+while True:
+    for ev in pygame.event.get():
+        if ev.type == pygame.QUIT:
+            pygame.quit()
+
+#change window color
